72126c1591501363f010af4aac58c9844037c770
laxerrrr <laxermario@gmail.com>
2020-02-02 19:54:10 -0400

    SDL2 framework functional

---
+++ woble/main.py
@@ -0,0 +1,23 @@
+import os
+import sys
+SDL2PATH = (os.getcwd() + "\\SDL2-2.0.10-win32-x64")
+os.environ["PYSDL2_DLL_PATH"] = SDL2PATH
+import sdl2
+import sdl2.ext
+
+def run():
+    sdl2.ext.init()
+    window = sdl2.ext.Window("Woble", size=(800, 600))
+    window.show()
+    running = True
+    while running:
+        events = sdl2.ext.get_events()
+        for event in events:
+            if event.type == sdl2.SDL_QUIT:
+                running = False
+                break
+        window.refresh()
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(run())
