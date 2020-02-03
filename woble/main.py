ee1fad7b23ddd218eda3543d471db1d4729d7d59
laxerrrr <laxermario@gmail.com>
2020-02-02 20:32:40 -0400

    if statement to check if in right directory

--- woble/main.py
+++ woble/main.py
@@ -1,23 +1,29 @@
 import os
 import sys
 SDL2PATH = (os.getcwd() + "\\SDL2-2.0.10-win32-x64")
-os.environ["PYSDL2_DLL_PATH"] = SDL2PATH
-import sdl2
-import sdl2.ext
+woblestring = SDL2PATH[-27:]S
 
-def run():
-    sdl2.ext.init()
-    window = sdl2.ext.Window("Woble", size=(800, 600))
-    window.show()
-    running = True
-    while running:
-        events = sdl2.ext.get_events()
-        for event in events:
-            if event.type == sdl2.SDL_QUIT:
-                running = False
-                break
-        window.refresh()
-    return 0
+if woblestring == "woble\\SDL2-2.0.10-win32-x64":
+    os.environ["PYSDL2_DLL_PATH"] = SDL2PATH
+    print(woblestring)
+    import sdl2
+    import sdl2.ext
 
-if __name__ == "__main__":
-    sys.exit(run())
+    def run():
+        sdl2.ext.init()
+        window = sdl2.ext.Window("Woble", size=(800, 600))
+        window.show()
+        running = True
+        while running:
+            events = sdl2.ext.get_events()
+            for event in events:
+                if event.type == sdl2.SDL_QUIT:
+                    running = False
+                    break
+            window.refresh()
+        return 0
+
+    if __name__ == "__main__":
+        sys.exit(run())
+else:
+    print("Use 'cd wobble' in terminal to switch to the correct folder.")
