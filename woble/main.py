b82028aa1dc8e39eb9b357b206559ae491f71d94
laxerrrr <laxermario@gmail.com>
2020-02-02 22:52:45 -0400

    comments to clarify code

--- woble/main.py
+++ woble/main.py
@@ -4,13 +4,14 @@ contents = (os.listdir(os.getcwd()))
 SDL2PATH = (os.getcwd() + "\\SDL2-2.0.10-win32-x64")
 if "main.py" and "SDL2-2.0.10-win32-x64" in contents :
     os.environ["PYSDL2_DLL_PATH"] = SDL2PATH
+    #Importing SDL2
     import sdl2
     import sdl2.ext
-
+    #Main process for SDL2
     def run():
-        sdl2.ext.init()
-        window = sdl2.ext.Window("Woble", size=(800, 600))
-        window.show()
+        sdl2.ext.init() #Start SDL2
+        window = sdl2.ext.Window("Woble [Alpha]", size=(800, 600))
+        window.show() #Show window
         running = True
         while running:
             events = sdl2.ext.get_events()
