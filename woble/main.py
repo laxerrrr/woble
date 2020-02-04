481e21da34e7f9fdcf8d73bac99b910b0d99f136
laxerrrr <laxermario@gmail.com>
2020-02-03 21:51:02 -0400

    functioning sprite renderer with world

--- woble/main.py
+++ woble/main.py
@@ -2,11 +2,26 @@ import os
 import sys
 contents = (os.listdir(os.getcwd()))
 SDL2PATH = (os.getcwd() + "\\SDL2-2.0.10-win32-x64")
-if "main.py" and "SDL2-2.0.10-win32-x64" in contents :
+if "main.py" and "SDL2-2.0.10-win32-x64" in contents : #Solely for development
     os.environ["PYSDL2_DLL_PATH"] = SDL2PATH
     #Importing SDL2
     import sdl2
     import sdl2.ext
+    #Custom classes for game
+    class Renderer (sdl2.ext.SoftwareSpriteRenderSystem) : #Renderer class for sprites
+        def __init__(self, window):
+            super(Renderer, self).__init__(window)
+        
+        def render(self, components):
+            sdl2.ext.fill(self.surface, sdl2.ext.Color(200, 200, 123)) #Background of renderer
+            super(Renderer, self).render(components) #Call SoftwareSpriteRenderSystem.render() when using Render.render()
+    
+    class Player (sdl2.ext.Entity) : #Player class
+        def __init__(self, world, sprite, posx = 0, posy = 0):
+            self.sprite = sprite #Given variable when making Player attribute to be associated with Player.sprite
+            self.sprite.position = posx, posy #Set posx posy to player sprite position
+
+
     #Main process for SDL2
     def run():
         sdl2.ext.init() #Start SDL2
@@ -15,11 +30,16 @@ if "main.py" and "SDL2-2.0.10-win32-x64" in contents :
         window.show() #Show window
         running = True
 
-        factory = sdl2.ext.SpriteFactory(sdl2.ext.SOFTWARE) #Sprite factory
-        player = factory.create_software_sprite(size=(500, 106), bpp=8)
+        #Add anything here
+        renderer = Renderer(window)
+        world = sdl2.ext.World()
+        world.add_system(renderer)
+        factory = sdl2.ext.SpriteFactory(sdl2.ext.SOFTWARE) #Sprite maker
+        first = factory.from_color(sdl2.ext.Color(0, 0, 255), size=(20, 20))
+        player = Player(world, first)
 
-        spriterenderer = factory.create_sprite_render_system(window) #Sprite renderer
-        spriterenderer.render(player)
+        renderer.render(first)
+
 
 
 
@@ -34,5 +54,5 @@ if "main.py" and "SDL2-2.0.10-win32-x64" in contents :
 
     if __name__ == "__main__":
         sys.exit(run())
-else :
+else : #Solely for development
     print("Use 'cd woble' in terminal to switch to the correct folder. (For VS Code)")
